e63690d0f5c5ca55a18e7316d1ec673ee874a4de
nextnerve0-byte <nextnerve0@gmail.com>
2026-04-23 18:21:40 +0500

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,16 +1,21 @@
 const Discord = require("discord.js");
-const config = require("../../config.json");
 
 module.exports = async (client) => {
-    await client.user.setPresence({
-        activities: [
-            {
-                name: `to ${config.Prefix}Play`,
-                type: Discord.ActivityType.Listening,
-            },
-        ],
-        status: "online",
-    });
+    const statuses = [
+        { name: `/play to listen to music 🎵`, type: Discord.ActivityType.Listening },
+        { name: `music in ${client.guilds.cache.size} servers`, type: Discord.ActivityType.Playing },
+        { name: `/help for commands`, type: Discord.ActivityType.Watching },
+        { name: `your favorite songs 🎶`, type: Discord.ActivityType.Listening },
+    ];
+
+    let i = 0;
+    setInterval(() => {
+        client.user.setPresence({
+            activities: [statuses[i]],
+            status: "online",
+        });
+        i = (i + 1) % statuses.length;
+    }, 10000);
 
     console.log(`${client.user.tag} is online and ready to play music for you!`);
 };
